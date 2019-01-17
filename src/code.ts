038162e4f0d6419dff92df24bad8b25d9567f6bb
mazahir89 <mazahir89@gmail.com>
2019-01-17 10:48:03 +0100

    Observable of differnet observers

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,23 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-console.log(Rx);
+var observable = Observable.create((observer: any) => {
+    observer.next("Hello!");
+    observer.next("How are you?");
+    observer.complete();
+    observer.next("This will not send!");
+});
+
+observable.subscribe(
+    (x: any) => addItem(x),
+    (error: any) => addItem(error),
+    () => addItem('Completed')
+);
+
+function addItem(val: any) {
+
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+
+}
