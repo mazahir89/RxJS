34b8874659a7e25a810ea7dd689f90808faf1fc6
mazahir89 <mazahir89@gmail.com>
2019-01-17 15:12:03 +0100

    Subscribing observable, unsubscribing observable. calling multiple observables and unsubscribing all when one of them are unsubscribed

--- src/code.ts
+++ src/code.ts
@@ -1,17 +1,37 @@
 import { Observable } from "rxjs/Observable";
 
 var observable = Observable.create((observer: any) => {
-    observer.next("Hello!");
-    observer.next("How are you?");
-    observer.complete();
-    observer.next("This will not send!");
+    try {
+
+        observer.next("Hello!");
+        observer.next("How are you?");
+        setInterval(() => {
+            observer.next("I am good");
+        }, 2000)
+        // observer.complete();
+        // observer.next("This will not send!");
+    }
+    catch (e) {
+        observer.error(e);
+
+    }
 });
 
-observable.subscribe(
+var observer = observable.subscribe(
     (x: any) => addItem(x),
     (error: any) => addItem(error),
     () => addItem('Completed')
 );
+
+var observer2 = observable.subscribe(
+    (x: any) => addItem(x)
+);
+
+observer.add(observer2);
+
+setTimeout(() => {
+    observer.unsubscribe();
+}, 6001);
 
 function addItem(val: any) {
 
