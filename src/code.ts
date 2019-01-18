946b5a159923d045c52cb47573550f021bc8234a
mazahir89 <mazahir89@gmail.com>
2019-01-18 09:36:19 +0100

    map operator use

--- src/code.ts
+++ src/code.ts
@@ -1,17 +1,33 @@
 import { Observable } from 'rxjs/Observable';
-import { merge } from "rxjs/observable/merge";
+//import { merge } from "rxjs/observable/merge";
 
-var observable = Observable.create((observer: any) => {
+import "rxjs/add/operator/map";
+
+
+Observable.create((observer: any) => {
     observer.next("Hello");
-});
+})
+
+    .map((val: any) => val.toUpperCase())
+    .subscribe((x: any) => addItem(x));
+
+
+
+/* var observable = Observable.create((observer: any) => {
+    observer.next("Hello");
+}); 
 
 var observable2 = Observable.create((observer: any) => {
-    observer.next("How are you");
+    observer.next("How are you?");
 });
 
 var newObs = merge(observable, observable2);
 
 newObs.subscribe((x: any) => addItem(x));
+*/
+
+
+
 
 function addItem(val: any) {
 
