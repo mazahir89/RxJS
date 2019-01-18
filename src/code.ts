85fdf9616a5321a48e8db33470275ec5d24db977
mazahir89 <mazahir89@gmail.com>
2019-01-18 08:43:45 +0100

    AsyncSubject last value to observers. only completes when complete is there

--- src/code.ts
+++ src/code.ts
@@ -1,31 +1,22 @@
-import { ReplaySubject } from "rxjs/ReplaySubject";
+import { AsyncSubject } from "rxjs/AsyncSubject";
 
-var subject = new ReplaySubject(30, 500);
+var subject = new AsyncSubject();
 
 subject.subscribe(
     data => addItem("Observer 1: " + data),
-    err => addItem(err),
     () => addItem("Observer 1 Completed")
 )
 var i = 1;
 var int = setInterval(() => subject.next(i++), 100);
 
-/* subject.next("The first thing has been sent")
-subject.next("Another thing has been sent")
-subject.next("...Observer 2 is about to subscribe...") */
 
 setTimeout(() => {
     var observer2 = subject.subscribe(
         data => addItem("Observer 2: " + data)
     )
+    subject.complete();
 }, 500);
 
-/* subject.next("The second thing has been sent");
-subject.next("A third thing has been sent");
-
-observer2.unsubscribe();
-
-subject.next("A final thing has been sent"); */
 
 function addItem(val: any) {
 
