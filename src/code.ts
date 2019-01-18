52e8c03ce3a18a2e5129e2d328539733a2788306
mazahir89 <mazahir89@gmail.com>
2019-01-18 08:38:14 +0100

    replaysubject 2 observers in milieseconds

--- src/code.ts
+++ src/code.ts
@@ -1,28 +1,31 @@
 import { ReplaySubject } from "rxjs/ReplaySubject";
 
-var subject = new ReplaySubject(2);
+var subject = new ReplaySubject(30, 500);
 
 subject.subscribe(
     data => addItem("Observer 1: " + data),
     err => addItem(err),
     () => addItem("Observer 1 Completed")
 )
+var i = 1;
+var int = setInterval(() => subject.next(i++), 100);
 
-subject.next("The first thing has been sent")
+/* subject.next("The first thing has been sent")
 subject.next("Another thing has been sent")
-subject.next("...Observer 2 is about to subscribe...")
+subject.next("...Observer 2 is about to subscribe...") */
 
-var observer2 = subject.subscribe(
-    data => addItem("Observer 2: " + data),
-)
+setTimeout(() => {
+    var observer2 = subject.subscribe(
+        data => addItem("Observer 2: " + data)
+    )
+}, 500);
 
-subject.next("The second thing has been sent");
+/* subject.next("The second thing has been sent");
 subject.next("A third thing has been sent");
-
 
 observer2.unsubscribe();
 
-subject.next("A final thing has been sent");
+subject.next("A final thing has been sent"); */
 
 function addItem(val: any) {
 
