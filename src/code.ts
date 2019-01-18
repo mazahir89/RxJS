737dc7779123191e78d88f2e6ba9cd380d979d75
mazahir89 <mazahir89@gmail.com>
2019-01-18 08:17:21 +0100

    observable from Subject

--- src/code.ts
+++ src/code.ts
@@ -1,14 +1,27 @@
-import { Observable } from "rxjs/Observable";
-import { fromEvent } from "rxjs/Observable/fromEvent";
+import { Subject } from "rxjs/Subject";
+
+var subject = new Subject();
+
+subject.subscribe(
+    data => addItem("Observer 1: " + data),
+    err => addItem(err),
+    () => addItem("Observer 1 Completed")
+)
+
+subject.next("The first thing has been sent")
+
+var observer2 = subject.subscribe(
+    data => addItem("Observer 2: " + data),
+)
+
+subject.next("The second thing has been sent");
+subject.next("A third thing has been sent");
 
 
-var observable = fromEvent(document, 'mousemove');
+observer2.unsubscribe();
 
-setTimeout(() => {
-    var subscription = observable.subscribe(
-        (x: any) => addItem(x)
-    )
-}, 2000)
+subject.next("A final thing has been sent");
+
 function addItem(val: any) {
 
     var node = document.createElement("li");
